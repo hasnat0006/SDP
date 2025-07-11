0a7dac7d44988a34c8cd70b66f2d5ae509c050a1
hasnat0006 <yusufrezahasnat0006@gmail.com>
2025-07-11 15:37:06 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mental Health Dashboard',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Poppins',
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MindOra'),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
