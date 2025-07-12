603f3e222f288a87a43fc398d988d8da8285c0cc
hasnat0006 <yusufrezahasnat0006@gmail.com>
2025-07-13 00:13:28 +0600

    Refactor constructors to use 'super.key' syntax and implement CustomBottomNavBar with navigation functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:client/journal/journal.dart';
+import 'package:client/journal/journal_history.dart';
 import 'package:flutter/material.dart';
  import 'mood/Mood_spin.dart';
 // import 'mood/Mood_intensity.dart';
 import './todo_list/todo_list_main.dart';
+import './navbar/navbar.dart';
+import './login/signup/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         fontFamily: 'Poppins',
       ),
-      home: const MoodPage(),
+      home: const LoginPage(),
     );
   }
-} 
+}
