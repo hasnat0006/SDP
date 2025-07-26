f60fb8dc8a801f62d7be5fb2fac6aea73fa772d7
zaaaiiimaaa <zaimaahmed101@gmail.com>
2025-07-26 18:46:05 +0600

    book appointment connected

--- lib/dashboard/p_dashboard.dart
+++ lib/dashboard/p_dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:client/appointment/bookappt.dart';
 import 'package:client/chatbot/startpage.dart';
 import 'package:client/forum/forum.dart';
 import 'package:client/journal/journal.dart';
@@ -252,6 +253,29 @@ class DashboardPage extends StatelessWidget {
           'Book an Appointment',
           'Get professional help',
           context,
+          onTap: () {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    const BookAppt(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                      const begin = Offset(1.0, 0.0);
+                      const end = Offset.zero;
+                      const curve = Curves.ease;
+                      final tween = Tween(
+                        begin: begin,
+                        end: end,
+                      ).chain(CurveTween(curve: curve));
+                      return SlideTransition(
+                        position: animation.drive(tween),
+                        child: child,
+                      );
+                    },
+              ),
+            );
+          },
         ),
         _trackerTile(
           Icons.edit_note,
