53ded846ffb3c1edac37dc422914da0f5feba1ee
zaaaiiimaaa <zaimaahmed101@gmail.com>
2025-07-27 00:06:57 +0600

    nabiha

--- android/app/src/main/kotlin/com/example/client/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/client/MainActivity.kt
@@ -1,30 +1,5 @@
-package com.nabiha.sleeptracker
+package com.example.client
 
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
-
-    private val CHANNEL = "com.yourapp/unlock"
-    private lateinit var unlockReceiver: UnlockReceiver
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        val channel = MethodChannel(flutterEngine.dartExecutor, CHANNEL)
-        unlockReceiver = UnlockReceiver(channel)
-
-        // Register receiver for unlock
-        val filter = IntentFilter(Intent.ACTION_USER_PRESENT)
-        registerReceiver(unlockReceiver, filter)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(unlockReceiver)
-    }
-}
+class MainActivity : FlutterActivity()
