24169141846232537c25464c2b6112997398d325
zaaaiiimaaa <zaimaahmed101@gmail.com>
2025-08-06 15:59:13 +0600

    forget pass and reset pass page

--- mindora/lib/login/signup/login.dart
+++ mindora/lib/login/signup/login.dart
@@ -3,6 +3,7 @@ import 'package:client/navbar/navbar.dart';
 import 'package:flutter/material.dart';
 import '../../dashboard/p_dashboard.dart';
 import 'signup1.dart';
+import 'forgetpass.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -226,9 +227,17 @@ class _LoginPageState extends State<LoginPage> {
   Widget _buildFooterText() {
     return Column(
       children: [
-        const Text(
-          "Forgot Password",
-          style: TextStyle(color: Colors.orange, fontWeight: FontWeight.w600),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ForgetPassPage()),
+            );
+          },
+          child: const Text(
+            "Forgot Password",
+            style: TextStyle(color: Colors.orange, fontWeight: FontWeight.w600),
+          ),
         ),
         const SizedBox(height: 6),
         Row(
