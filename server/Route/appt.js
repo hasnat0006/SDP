d2b057fbf9c50796df934dabd3597a2d1565ed49
nabiha45 <128611113+nabiha45@users.noreply.github.com>
2025-08-15 15:31:28 +0600

    .

--- server/Route/appt.js
+++ server/Route/appt.js
@@ -4,12 +4,39 @@ const router = express.Router();
 
 // Fetching therapist details
 
-router.get("/therapists", (req, res) => {
-  sql.query('SELECT * FROM users WHERE type = "doctor"', (err, results) => {
-    if (err) {
-      res.status(500).json({ error: "Error fetching data" });
-      return;
+router.get("/therapists", async (req, res) => {
+  try {
+    const result = await sql`
+        SELECT * from users where type = 'doctor'`;
+    if (result.length === 0) {
+      return res.status(404).json({ error: "No therapists found" });
     }
-    res.status(200).json(results); // Return therapist data in JSON format
-  });
+    res.json(result[0]);
+    console.log(result[0]);
+  } catch (err) {
+    console.error("Error retrieving therapist data:", err);
+    res.status(500).json({ error: err.message });
+  }
 });
+// router.get("/data/:userId", async (req, res) => {
+//   try {
+//     const { userId } = req.params;
+//     console.log("User ID:", userId);
+//     const result = await sql`
+//       SELECT * FROM stress_tracker
+//       WHERE user_id = ${userId}
+//       ORDER BY date DESC
+//       LIMIT 1
+//     `;
+
+//     if (result.length === 0) {
+//       return res.status(404).json({ error: "No stress data found" });
+//     }
+
+//     res.json(result[0]);
+//   } catch (err) {
+//     console.error("Error retrieving stress data:", err);
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+module.exports = router;
